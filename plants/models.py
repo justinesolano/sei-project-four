73e1fc5b26b43b0e1a07289545c48bb70ed9ca6a
jsolano <justinesolano2370@gmail.com>
2021-04-16 11:37:13 +0100

    populating owners of plants

--- plants/models.py
+++ plants/models.py
@@ -11,6 +11,11 @@ class Plant(models.Model):
     decorative_bonus = models.PositiveIntegerField()
     average_price = models.PositiveIntegerField()
     categories = models.ManyToManyField('categories.Categories', related_name="plants")
+    owner = models.ForeignKey(
+        'jwt_auth.User',
+        related_name='created_meals',
+        on_delete = models.CASCADE
+    )
 
     def __str__(self):
         return f"{self.plant_name} - {self.scientific_name}"
--- plants/serializers/populated.py
+++ plants/serializers/populated.py
@@ -1,8 +1,8 @@
-from comments.serializers.common import CommentSerializer
+from comments.serializers.populated import PopulatedCommentSerializer
 from ..serializers.common import PlantSerializer
 from categories.serializers.common import CategoriesSerializer
 
 class PopulatedPlantSerializer(PlantSerializer):
 
-    comments = CommentSerializer(many=True)
+    comments = PopulatedCommentSerializer(many=True)
     categories = CategoriesSerializer(many=True)
