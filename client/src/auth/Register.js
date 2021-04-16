edd932a6a136441e1fa22b378f61103fa49c3232
jsolano <justinesolano2370@gmail.com>
2021-04-16 16:24:50 +0100

    decided to solely use semantic ui for css, installed it, started on register page

--- client/src/auth/Register.js
+++ client/src/auth/Register.js
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom'
+import axios from 'axios'
 
 const Register = () => {
+
+  const [form, setForm] = useState({
+    username: '',
+    email: '',
+    password: '',
+    passwordConfirmation: '',
+  })
+
   return (
     <div>
       <h1>Register</h1>
