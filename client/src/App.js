6fe26dde6194ba76bd96edd0713e746bcc4b6650
jsolano <justinesolano2370@gmail.com>
2021-04-16 13:15:25 +0100

    victory! got console log to show the plants in await fetch, wasn't working due to invalid auth token even with isauthenticatedorreadonly stack overflow told me it was because of my cookies so deleted them and it worked

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,17 @@
-function App() {
+import React, { useEffect } from 'react'
+
+const App = () => {
+
+  useEffect(() => {
+    const getData = async () => {
+      const res = await fetch('/api/plants')
+      console.log(await res.json())
+    }
+    getData()
+  }, [])
+
   return <h1>Hello World</h1>
+
 }
 
 export default App
