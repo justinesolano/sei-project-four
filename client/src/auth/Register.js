ed5ad911018eaf55e89f54483ed651cf91140225
jsolano <justinesolano2370@gmail.com>
2021-04-19 18:23:49 +0100

    register not working again! everything looks right but post doesn't like history.push after registering

--- client/src/auth/Register.js
+++ client/src/auth/Register.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-// import { useHistory } from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 import axios from 'axios'
 
 const Register = () => {
@@ -12,7 +12,7 @@ const Register = () => {
     password_confirmation: '',
   })
 
-  // const history = useHistory()
+  const history = useHistory()
 
   const handleChange = (event) => {
     const newForm = { ...form, [event.target.name]: event.target.value }
@@ -23,8 +23,7 @@ const Register = () => {
     event.preventDefault()
     try {
       // await axios.post('/api/auth/register/', form)
-      await axios.post('/api/auth/register/', form)
-      const response = await axios.post('/api/auth/login/', { email: form.email, password: form.password })
+      const response = await axios.post('/api/auth/register/', form)
       window.localStorage.setItem('token', response.data.token)
       history.push('/')
     } catch (err) {
@@ -43,77 +42,80 @@ const Register = () => {
   // password/confirmation
 
   return (
-    <body className="register-body">
-      <form className="ui form" onSubmit={handleSubmit}>
-        <div className="required field">
-          <label>Email</label>
-          <input
-            className={`input ${errors}`}
-            placeholder="Email"
-            type="text"
-            name="email"
-            value={form.email}
-            onChange={handleChange}  
-          />
-        </div>
-        <div className="required field">
-          <label>First Name</label>
-          <input
-            className="register-input"
-            placeholder="First Name"
-            type="text"
-            name="first_name"
-            value={form.firstName}
-            onChange={handleChange}  
-          />
-        </div>
-        <div className="field">
-          <label>Last Name</label>
-          <input
-            className="register-input"
-            placeholder="Last Name"
-            type="text"
-            name="last_name"
-            value={form.lastName}
-            onChange={handleChange}  
-          />
-        </div>
-        <div className="required field">
-          <label>Username</label>
-          <input
-            className="register-input"
-            placeholder="Username"
-            type="text"
-            name="username"
-            value={form.username}
-            onChange={handleChange}  
-          />
-        </div>
-        <div className="required field">
-          <label>Password</label>
-          <input
-            className="register-input"
-            placeholder="Password"
-            type="password"
-            name="password"
-            value={form.password}
-            onChange={handleChange}  
-          />
-        </div>
-        <div className="required field">
-          <label>Confirm your password</label>
-          <input
-            className="register-input"
-            placeholder="Password Confirmation"
-            type="password"
-            name="password_confirmation"
-            value={form.passwordConfirmation}
-            onChange={handleChange}  
-          />
-        </div>
-        <button className="ui green button" type="submit">Submit!</button>
-      </form>
-    </body>
+    <>
+      <h1>Register</h1>
+      <body className="register-body">
+        <form className="ui form" onSubmit={handleSubmit}>
+          <div className="required field">
+            <label>Email</label>
+            <input
+              className={`input ${errors}`}
+              placeholder="Email"
+              type="text"
+              name="email"
+              value={form.email}
+              onChange={handleChange}  
+            />
+          </div>
+          <div className="required field">
+            <label>First Name</label>
+            <input
+              className="register-input"
+              placeholder="First Name"
+              type="text"
+              name="first_name"
+              value={form.firstName}
+              onChange={handleChange}  
+            />
+          </div>
+          <div className="field">
+            <label>Last Name</label>
+            <input
+              className="register-input"
+              placeholder="Last Name"
+              type="text"
+              name="last_name"
+              value={form.lastName}
+              onChange={handleChange}  
+            />
+          </div>
+          <div className="required field">
+            <label>Username</label>
+            <input
+              className="register-input"
+              placeholder="Username"
+              type="text"
+              name="username"
+              value={form.username}
+              onChange={handleChange}  
+            />
+          </div>
+          <div className="required field">
+            <label>Password</label>
+            <input
+              className="register-input"
+              placeholder="Password"
+              type="password"
+              name="password"
+              value={form.password}
+              onChange={handleChange}  
+            />
+          </div>
+          <div className="required field">
+            <label>Confirm your password</label>
+            <input
+              className="register-input"
+              placeholder="Password Confirmation"
+              type="password"
+              name="password_confirmation"
+              value={form.passwordConfirmation}
+              onChange={handleChange}  
+            />
+          </div>
+          <button className="ui green button" type="submit">Submit!</button>
+        </form>
+      </body>
+    </>
   )
 }
 
