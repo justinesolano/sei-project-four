3572eeed4c8b537151bfa20bc6457fac88a9be80
jsolano <justinesolano2370@gmail.com>
2021-04-19 12:02:14 +0100

    yarn added whirligig from react for home slider

--- client/src/components/slide/Slider.js
+++ client/src/components/slide/Slider.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
-import { Slide } from 'react-slideshow-image'
+// import { Slide } from 'react-slideshow-image'
+import Whirligig from 'react-whirligig'
 import SlideShow from './SlideShow'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
@@ -21,6 +22,10 @@ const Slider = ({ plantname }) => {
     getData()
   }, [])
 
+  let whirligig
+  const next = () => whirligig.next()
+  const prev = () => whirligig.prev()
+
   // error conditional
 
   // if (!plants) return null
@@ -28,13 +33,29 @@ const Slider = ({ plantname }) => {
   return (
     <div className="slider-container">
       {plants &&
-        <Slide easing="ease">
-          {plants.map(plant => (
-            <Link to={`/${plant.plantname}`} key={`/${plant.id}`}  >
-              <SlideShow key={plantname} {...plant} />
-            </Link>
-          ))}
-        </Slide>
+            <div>
+              <button onClick={prev}>Prev</button>
+              <Whirligig
+                visibleSlides={3}
+                gutter="1em"
+                ref={(_whirligigInstance) => { 
+                  whirligig = _whirligigInstance 
+                }}>
+                {plants.map(plant => (
+                  <Link to={`/${plant.plantname}`} key={`/${plant.id}`}  >
+                    <SlideShow key={plantname} {...plant} />
+                  </Link>
+                ))}
+              </Whirligig>
+              <button onClick={next}>Next</button>
+            </div>
+        // <Slide easing="ease">
+        //   {plants.map(plant => (
+        //     <Link to={`/${plant.plantname}`} key={`/${plant.id}`}  >
+        //       <SlideShow key={plantname} {...plant} />
+        //     </Link>
+        //   ))}
+        // </Slide>
       }
     </div>
   )
