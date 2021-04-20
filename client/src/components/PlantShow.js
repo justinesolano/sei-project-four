9114ef70c38a475f00e4716aaeb3b4b519b3bfd2
jsolano <justinesolano2370@gmail.com>
2021-04-20 13:08:59 +0100

    plant show rendering successfully now, moving onto user profile

--- client/src/components/PlantShow.js
+++ client/src/components/PlantShow.js
@@ -5,31 +5,44 @@ import { useParams } from 'react-router-dom'
 const PlantShow = () => {
 
   const [plant, setPlant] = useState(null)
-  const { id } = useParams()
+  const params = useParams()
   // (/api/plants/${id}/`)
 
   useEffect(() => {
     const getData = async () => {
+      console.log('PARAMS', params)
       try {
-        const { data } = await axios.get(`/api/plants/${id}/`)
+        const { data } = await axios.get(`/api/plants/${params.id}`)
         setPlant(data)
-        console.log('PLANTS DATA RESPONSE', data.plantname)
+        console.log('PLANTS DATA RESPONSE', data)
       } catch (err) {
         console.log(err)
       }
     }
     getData()
-  }, [])
+  }, [params.id])
 
   console.log('PLANTS ID', plant)
 
-  const { plantname, image } = plant
 
   return (
-    <div>
-      <h1 className="plant-name">{plantname}</h1>
-      <img src={image} alt={plantname} className="plant-image"/>
-    </div>
+    <>
+      { plant ? 
+        <div>
+          <h1 className="plant-name">{plant.plantname}</h1>
+          <h2> {plant.scientificname} </h2>
+          <h2> {plant.family} </h2>
+          <img src={plant.image} alt={plant.plantname} className="plant-image"/>
+          <p> {plant.size} </p> <p> {plant.maintenancelevel} </p>
+          <p> Best suited: {plant.bestsuited} </p>
+          <p> {plant.averageprice} </p>
+          <p> {plant.description} </p>
+          <p> {plant.careinstructions} </p>
+        </div>
+        :
+        <h1>Something went wrong!</h1>
+      }
+    </>
   )
 }
 
--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -1,10 +1,40 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+
 
 const UserProfile = () => {
+
+  const [profile, setProfile] = useState(null)
+  const { id } = useParams()
+
+  useEffect(() => {
+    const getData = async () => {
+      // console.log('PARAMS', params)
+      try {
+        const { data } = await axios.get(`/api/auth/profile/${id}`)
+        setProfile(data)
+        console.log('PROFILE DATA', data)
+      } catch (err){
+        console.log(err)
+      }
+    }
+    getData()
+  }, [id])
+
+  console.log('PLANTS ID', profile)
+
   return (
-    <div>
-      <h1>UserProfile</h1>
-    </div>
+    <>
+      { profile ?
+        <div>
+          <h1>UserProfile</h1>
+          <h2> {profile.first_name} </h2>
+        </div>
+        :
+        <h1>User does not exist</h1>  
+      }
+    </>
   )
 }
 
