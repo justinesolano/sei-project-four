a3a07e9d6fa069bb2610a06b4ddd48503946922c
jsolano <justinesolano2370@gmail.com>
2021-04-21 18:02:50 +0100

    adding image object to category modell

--- categories/models.py
+++ categories/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Categories(models.Model):
     name = models.CharField(max_length=50)
+    image = models.CharField(max_length=1000)
 
     def __str__(self):
         return f"{self.name}"
