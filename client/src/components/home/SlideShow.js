b338a5fdd4ac4136fd00e8cc4818e03e94453fa2
jsolano <justinesolano2370@gmail.com>
2021-04-21 18:37:05 +0100

    tried carousel

--- client/src/components/home/SlideShow.js
+++ client/src/components/home/SlideShow.js
@@ -3,13 +3,7 @@ import React from 'react'
 const  SlideShow = ( { image } ) => {
   
   return (
-    <div className="each-slide">
-      <div>
-        <span> 
-          <img src={image} className="slide-image"/>
-        </span>
-      </div>
-    </div>
+    <img src={image}/>
   )
 }
 
--- client/src/components/home/Sliders.js
+++ client/src/components/home/Sliders.js
@@ -4,7 +4,8 @@ import React, { useState, useEffect } from 'react'
 import SlideShow from './SlideShow'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
-import Slider from 'react-slick'
+import Carousel from 'react-bootstrap/Carousel'
+// import Slider from 'react-slick'
 // import 'slick-carousel/slick/slick.css'
 // import 'slick-carousel/slick/slick-theme.css'
 
@@ -25,35 +26,37 @@ const Sliders = () => {
     getData()
   }, [])
 
-  const config = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    centerMode: true,
-    centerPadding: '50px',
-    focusOnSelect: true,
-    dragable: true,
-    autoplay: true,
-    autoplaySpeed: 3000,
-  }
+  // const config = {
+  //   dots: true,
+  //   infinite: true,
+  //   speed: 500,
+  //   slidesToShow: 3,
+  //   slidesToScroll: 1,
+  //   centerMode: true,
+  //   centerPadding: '50px',
+  //   focusOnSelect: true,
+  //   dragable: true,
+  //   autoplay: true,
+  //   autoplaySpeed: 3000,
+  // }
 
 
   // error conditional
   if (!plants) return null
 
   return (
-    <div className="slider-container">
+    <div>
       {plants &&
           <div>
-            <Slider {...config}>
+            <Carousel>
               {plants.map(plant => (
                 <Link to={`/plants/${plant.id}`} key={`/${plant.id}`}  >
-                  <SlideShow key={plant.plantname} {...plant} />
+                  <Carousel.Item>
+                    <SlideShow key={plant.plantname} {...plant} />
+                  </Carousel.Item>
                 </Link>
               ))}
-            </Slider>
+            </Carousel>
           </div>
       }
     </div>
