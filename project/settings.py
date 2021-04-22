36fa9e3e43eabe73b0203ff2d417ff71c6e136b9
jsolano <justinesolano2370@gmail.com>
2021-04-22 19:43:15 +0100

    ready to deploy

--- project/settings.py
+++ project/settings.py
@@ -1,3 +1,5 @@
+import os
+import django_on_heroku
 """
 Django settings for project project.
 
@@ -59,7 +61,9 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'client')
+                 ]  #Look, we have added the root folder of frontend here
+        ,
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -141,3 +145,13 @@ REST_FRAMEWORK = {
         'jwt_auth.authentication.JWTAuthentication'
     ],
 }
+
+ROOT_URLCONF = 'project.urls' #check if you have this already, if not add it in
+
+STATIC_URL = '/static/' # same with this
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'frontend', "build", "static"),
+)
+
+django_on_heroku.settings(locals())
--- project/urls.py
+++ project/urls.py
@@ -14,7 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from .views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,4 +24,5 @@ urlpatterns = [
     path('api/categories/', include('categories.urls')),
     path('api/auth/', include('jwt_auth.urls')),
     path('api/posts/', include('posts.urls')),
+    re_path(r'^.*$', index)
 ]
