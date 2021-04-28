d60b5770968204f54bd0f94876746b7875c6ee0a
jsolano <justinesolano2370@gmail.com>
2021-04-28 18:31:38 +0100

    deploying again

--- project/settings.py
+++ project/settings.py
@@ -151,7 +151,7 @@ ROOT_URLCONF = 'project.urls' #check if you have this already, if not add it in
 STATIC_URL = '/static/' # same with this
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'frontend', "build", "static"),
+    os.path.join(BASE_DIR, 'client', "build", "static"),
 )
 
 django_on_heroku.settings(locals())
